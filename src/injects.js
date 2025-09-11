0b760d3fedb89444a3a935d7fcd11c8941a6717b
evilcorn128 <evilcorn128@gmail.com>
2025-09-11 08:21:12 +0000

    mas juegos!

--- src/injects.js
+++ src/injects.js
@@ -1,10 +1,22 @@
 window.JavaScript = () => {
+    const script = document.createElement("script");
+script.textContent = `
+  (function() {
     const base = document.querySelector("base");
     if (!base) return;
     const baseHref = base.href;
     Object.defineProperty(document, "URL", { value: baseHref, configurable: true });
     Object.defineProperty(document, "baseURI", { value: baseHref, configurable: true });
-    try { Object.defineProperty(window.location, "href", { get: () => baseHref, configurable: true }); } catch (_) {}
+    try { 
+      Object.defineProperty(window.location, "href", { 
+        get: () => baseHref, 
+        configurable: true 
+      }); 
+    } catch (_) {}
+  })();
+`;
+document.head.appendChild(script);
+
 };
 
 window.CSS = `
@@ -15,11 +27,10 @@ window.CSS = `
 
 window.CSS_overrides = {
     "https://warpedentrance2.github.io/crazya.github.io/games/a-dance-of-fire-and-ice/": `
-    canvas {
-        width: 100vw !important;
-        height: 100vh !important;
-    }
-`
+        canvas {
+            width: 100vw !important;
+            height: 100vh !important;
+        }`,
 };
 
 window.JavaScript_overrides = {
